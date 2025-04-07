845a11a48ca164c34be3036fb5a68b5f5169e211
GromazekaS <kinst41@gmail.com>
2025-04-07 21:17:48 +0300

    mypy fixes

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,7 +1,9 @@
 from src.masks import get_mask_account, get_mask_card_number
 
-def test_mask_account():
+
+def test_mask_account() -> None:
     assert get_mask_account(35383033474447895560) == "**5560"
 
-def test_mask_card():
+
+def test_mask_card() -> None:
     assert get_mask_card_number(7158300734726758) == "7158 30** **** 6758"

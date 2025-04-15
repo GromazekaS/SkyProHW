1cc97c7af341ec9e6dba9c880e9a6b7cc3b60d44
GromazekaS <kinst41@gmail.com>
2025-04-15 20:26:15 +0300

    add docstrings

--- src/generators.py
+++ src/generators.py
@@ -2,16 +2,19 @@ import typing
 
 
 def filter_by_currency(transactions: list[dict], currency_code: str = "USD") -> typing.Generator[dict]:
+    """Отфильтровать список транзакций по полю кода валюты"""
     return iter(x for x in transactions if x["operationAmount"]["currency"]["code"] == currency_code)
 
 
 def transactions_description(transactions: list[dict]) -> typing.Generator[str]:
+    """Выдавать по одному описание транзакции из всего списка"""
     gen = (x["description"] for x in transactions)
     while True:
         yield next(gen, "")
 
 
 def card_number_generator(first: int, last: int) -> typing.Generator[str]:
+    """Сгенерировать набор номеров карт в заданном диапазоне"""
     if first > last:
         raise ValueError("Wrong card number order. First card number is greater than last one")
     for n in range(first, last + 1):

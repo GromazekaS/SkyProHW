2684bcdbafa86483a4eac749d7daad800e248a0b
GromazekaS <kinst41@gmail.com>
2025-05-03 10:57:46 +0300

    add logging in widget.py INFO & ERROR

--- src/widget.py
+++ src/widget.py
@@ -1,26 +1,53 @@
-from src.decorators import log, timing
+import logging
+# from src.decorators import log, timing
 from src.masks import get_mask_account, get_mask_card_number
 
 
-@log("log_widget.txt")
-@timing
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('..\logs\widget.log', "w",encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
+# @log("log_widget.txt")
+# @timing
 def mask_account_card(data: str) -> str:
     """Завернуть номер счета или карты в маску"""
+    mask = ''
     temp = data.split()
     if temp[0] == "Счет":
-        mask = get_mask_account(int(temp[-1]))
+        logger.info(f"Маскирую номер счета")
+        try:
+            mask = get_mask_account(int(temp[-1]))
+        except Exception as ex:
+            logger.error(f"Возникла ошибка {ex} при попытке маскировать счет {data}")
+            return ''
     else:
-        mask = get_mask_card_number(int(temp[-1]))
+        logger.info(f"Маскирую номер карты {temp[:-1]}")
+        try:
+            mask = get_mask_card_number(int(temp[-1]))
+        except Exception as ex:
+            logger.error(f"Возникла ошибка {ex} при попытке маскировать счет {data}")
+            return ''
     result = data[: len(data) - len(temp[-1])] + mask
+    logger.info("Маскирование успешно завершено")
     return result
 
 
-@log(None)
-@timing
+# @log(None)
+# @timing
 def get_date(date: str) -> str:
     """Вернуть дату в формате ДД.ММ.ГГГГ"""
-    temp = date.split("T")[0].split("-")
-    result = ""
-    for item in temp[::-1]:
-        result += item + "."
-    return result[:-1]
+    result =''
+    try:
+        logger.info("Преобразуем формат даты")
+        temp = date.split("T")[0].split("-")
+        result = ""
+        for item in temp[::-1]:
+            result += item + "."
+        result = result[:-1]
+    except Exception as ex:
+        logger.error(f"Возникла ошибка {ex} при преобразовании строки {date} в дату")
+    return result
